74003fd4a92b0d942909377434c4e92e54c10477
simran9630 <simran@optacloud.in>
2024-04-25 16:25:56 +0530

    yaml modified8

--- GruntFile.cjs
+++ GruntFile.cjs
@@ -1,16 +1,24 @@
 module.exports = function(grunt) {
     // Configure grunt
+    require('@babel/register')({
+      // Load Babel configuration from .babelrc file
+      babelrc: true
+    });
     grunt.initConfig({
       mochaTest: {
         test: {
           options: {
             reporter: 'spec', // Use 'spec' reporter for Mocha
-            timeout: 5000 // Set test timeout (optional)
+            timeout: 5000,
+            require: 'esm' // Set test timeout (optional)
           },
-          src: ['test/**/*.js'] // Specify test files or directories
+          src: ['test.js'] ,
+         
         }
       }
     });
+   
+  
   
     // Load the grunt-mocha-test plugin
     grunt.loadNpmTasks('grunt-mocha-test');
